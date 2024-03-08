507320bc764f1adebb392348f844d91305dc8834
mcldrchl <128703392+mcldrchl@users.noreply.github.com>
2024-03-08 11:37:15 +0100

    custom filters and instructions

--- app.R
+++ app.R
@@ -1,10 +1,11 @@
 # Author: Rachel McLeod
 # rachel.mcleod@eawag.ch
-
 library(shiny)
 library(tidyverse)
 library(readxl)
 library(plotly)
+library(htmltools)
+library(shinyjs)
 
 # Create custom plot theme
 theme_cowwid <- function(base_size = 2) {
@@ -89,63 +90,148 @@ Fluos <- Spectra %>%
   filter(Type == "Emission") %>%
   arrange(Wavelength)
 
-fluo_palette <- grDevices::rainbow(length(unique(Fluos$Fluorophore)), start = 0, end = .7, rev = TRUE)
+fluo_palette <- grDevices::rainbow(length(unique(Fluos$Fluorophore)), start = 0, end = .65, rev = TRUE)
 fluo_palette <- setNames(fluo_palette, unique(Fluos$Fluorophore))
 
 # Define UI
 ui <- fluidPage(
-
-    # Application title
-    titlePanel("Fluorophore Spectra Investigator"),
-
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            selectInput("Prism",
-                        "Select Prism machine:",
-                        choices = c("Prism3", "Prism6", "Custom")),
-            selectInput("Type",
-                        "Select Spectrum Type:",
-                        choices = c("Emission", "Excitation")),
-            checkboxGroupInput("Fluorophores",
-                               "Select fluorophores to display:",
-                               choices = unique(Spectra$Fluorophore))
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("FluoPlot")
-        )
+  
+  # Application title
+  titlePanel("Fluorophore Spectra Investigator"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectInput("Prism",
+                  "Select machine:",
+                  choices = c("Prism3", "Prism6", "Custom")),
+      conditionalPanel(condition = "input.Prism == 'Custom'",
+                       numericInput("filtnum", 
+                                    "Enter the number of filters the machine contains", 
+                                    value = 3, 
+                                    min = 1, 
+                                    step = 1),
+                       uiOutput("customFilt")),
+      selectInput("Type",
+                  "Select Spectrum Type:",
+                  choices = c("Emission", "Excitation")),
+      checkboxGroupInput("Fluorophores",
+                         "Select fluorophores to display:",
+                         choices = unique(Spectra$Fluorophore))
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      fluidRow(
+        plotOutput("FluoPlot")
+      ),
+      fluidRow(
+        h4("About this tool"),
+        HTML(readLines("instructions.txt"))
+      ),
+      fluidRow(tableOutput("table"))
     )
+  )
+  
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
+  
+  output$customFilt <- renderUI({
+    
+    num <- as.integer(input$filtnum)
+    
+    lapply(1:num, function(i) {
+      splitLayout(
+        numericInput(paste0("input_", i, "_min"), 
+                     label = paste0("Enter filter ", i, " minimum wavelength"), 
+                     value = 450),
+        numericInput(paste0("input_", i, "_max"), 
+                     label = paste0("Enter filter ", i, " maximum wavelength"), 
+                     value = 500)
+      )
+      
+    })
+    
+  })
+  
+  customData <- reactive({
+    
+    num <- as.integer(input$filtnum)
+    
+    customData <- data.frame("filtmin" = integer(), 
+                             "filtmax" = integer(), 
+                             "filtname" = character())
+    
+    for (i in 1:num) {
+      customData[nrow(customData)+1,] <- c(input[[paste0("input_", i, "_min")]],
+                                           input[[paste0("input_", i, "_max")]],
+                                           paste("filter", i))
+    }
+    
+    customData
+    
+  })
+  
+  output$table <- renderTable({
+    customData()
+  })
+  
   output$FluoPlot <- renderPlot({
     
     if (input$Prism == "Prism3") {
       colours = rainbow[c(7, 5, 1)]
-    } else {
+    } else if (input$Prism == "Prism6") {
       colours = rainbow[c(7, 5, 1, 2, 6, 3)]
+    } else {
+      colours = grDevices::rainbow(input$filtnum, start = 0, end = .65, rev = TRUE)
     }
     
-    ggplot(filter(Spectra, Fluorophore %in% input$Fluorophores, Type == input$Type), aes(x = Wavelength, y = RelativeIntensity, color = Fluorophore)) +
-    geom_line(size = 1) +
-    geom_rect(data = filter(Filters, Prism == input$Prism, Type == input$Type), inherit.aes = FALSE,
-              aes(xmin = filtmin, xmax = filtmax, 
-                  ymin = -Inf, ymax = Inf, 
-                  fill = factor(filtname)), alpha = 0.2) +
-    scale_fill_manual(values = colours) +
-    scale_color_manual(values = fluo_palette) +
-    labs(x = "Wavelength [nm]", 
-         y = "Relative Intensity",
-         fill = "Filter",
-         colour = "Fluorophore") +
-    xlim(400, 800) +
-    theme_cowwid()
+    if (input$Prism != "Custom") {
+      ggplot() +
+        geom_rect(data = filter(Filters, Prism == input$Prism, Type == input$Type), 
+                  aes(xmin = filtmin, xmax = filtmax, 
+                      ymin = -Inf, ymax = Inf, 
+                      fill = factor(filtname)), alpha = 0.2) +
+        geom_line(data = filter(Spectra, Fluorophore %in% input$Fluorophores, Type == input$Type), 
+                  aes(x = Wavelength, 
+                      y = RelativeIntensity, 
+                      color = Fluorophore),
+                  linewidth = 1) +
+        scale_fill_manual(values = colours) +
+        scale_color_manual(values = fluo_palette) +
+        labs(x = "Wavelength [nm]", 
+             y = "Relative Intensity",
+             fill = "Filter",
+             colour = "Fluorophore") +
+        xlim(400, 800) +
+        ylim(0, 100) +
+        theme_cowwid()
+    } else {
+      ggplot() +
+        geom_rect(data = customData(),
+                  aes(xmin = as.numeric(filtmin), xmax = as.numeric(filtmax), 
+                      ymin = -Inf, ymax = Inf, 
+                      fill = factor(filtname)), alpha = 0.2) +
+        geom_line(data = filter(Spectra, Fluorophore %in% input$Fluorophores, Type == input$Type), 
+                  aes(x = Wavelength, 
+                      y = RelativeIntensity, 
+                      color = Fluorophore),
+                  linewidth = 1) +
+        scale_fill_manual(values = colours) +
+        scale_color_manual(values = fluo_palette) +
+        labs(x = "Wavelength [nm]", 
+             y = "Relative Intensity",
+             fill = "Filter",
+             colour = "Fluorophore") +
+        xlim(400, 800) +
+        ylim(0, 100) +
+        theme_cowwid()
+    }
 
     })
+  
 }
 
 # Run the application 
